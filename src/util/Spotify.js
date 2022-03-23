5a57c002a025d120f876dd239e8ca40e1949f68a
Siskyy <marcbertelli7@gmail.com>
2022-03-23 11:43:58 +0000

    SavePlaylistName method to spotify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -52,9 +52,25 @@ const Spotify = {
     },
 
     savePlaylist(playlistName, trackUris) {
-        if (!playlistName || !trackUris) /* checks if there is a playlistname or any tracks added */ {
+        if (!playlistName || !trackUris.length) /* checks if there is a playlistname or any tracks added */ {
             return;
         } 
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Auhtorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => response.json()
+        ).then(jsonResponse => {userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
+            {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({ name: playlistName})
+            }).then(response => response.json()
+            ).then(jsonResponse => {const playlistId = jsonResponse.id;})
+        });
+
     }
 
     };
